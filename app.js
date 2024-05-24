c49bf0f9695568c8095caf16819e630b84bc3812
nikitakesharwani <nikitakesharwani1708@gmail.com>
2024-05-24 13:39:58 +0530

    Episode-03

--- app.js
+++ app.js
@@ -1,3 +1,36 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+//using React.createElement-JS Object => Html Element(render)
+const heading = React.createElement("h1", { id: "heading" }, "Hello World!");
+console.log(heading);
+
+//using JSX=> Babel convert it to-React.createElement-JS Object => Html Element(render)
+const jsxHeading = (
+  <h1 id="heading" className="head">
+    Hello World!
+  </h1>
+);
+console.log(jsxHeading);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(jsxHeading);
+
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading" },
+//   React.createElement("div", { id: "parent" }, [
+//     React.createElement("div", { id: "child1" }, [
+//       React.createElement("h1", {}, "Hey I am h1 tag"),
+//       React.createElement("h2", {}, "Hi I am h2 tag"),
+//     ]),
+//     React.createElement("div", { id: "child2" }, [
+//       React.createElement("h1", {}, "Hi I am h1 tag"),
+//       React.createElement("h2", {}, "Hi I am h2 tag"),
+//     ]),
+//   ])
+// );
+
 {
   /* <div id="parent">
   <div id="child1">
@@ -10,22 +43,3 @@
   </div>
 </div>; */
 }
-
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  React.createElement("div", { id: "parent" }, [
-    React.createElement("div", { id: "child1" }, [
-      React.createElement("h1", {}, "Hi I am h1 tag"),
-      React.createElement("h2", {}, "Hi I am h2 tag"),
-    ]),
-    React.createElement("div", { id: "child2" }, [
-      React.createElement("h1", {}, "Hi I am h1 tag"),
-      React.createElement("h2", {}, "Hi I am h2 tag"),
-    ]),
-  ])
-);
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(heading);
